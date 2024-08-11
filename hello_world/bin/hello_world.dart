7b1002ac9357eb68bdb4cf06ad9e7b62971929d1
TH3-F001 <thef00l@f00lsgold.com>
2024-08-11 13:12:33 -0700

    first commit

---
+++ hello_world/bin/hello_world.dart
@@ -0,0 +1,17 @@
+
+void main() {
+
+  String name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flyByObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': 'https://nssdc.gsfc.nasa.gov/planetary/image/saturn.jpg'
+  };
+  print('Hello $name! the year is $year');
+  print('Antenna Girth: $antennaDiameter Nice.😏');
+  print('Flying by: $flyByObjects');
+  print('image $image');
+
+}
